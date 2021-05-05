8eb2b4e6301223a3e3c4972f523a158ec80f290b
yash10800 <54960592+yash10800@users.noreply.github.com>
2021-05-05 19:46:10 +0530

    Added if condition

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, error: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ error: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.error && !this.state.lat)
+      return <div>Error: {this.state.error}</div>;
+    else if (!this.state.error && this.state.lat)
+      return <div>Latitude: {this.state.lat}</div>;
+    else if (!this.state.error && !this.state.lat) return <div>Loading</div>;
+    else return null;
   }
 }
 
